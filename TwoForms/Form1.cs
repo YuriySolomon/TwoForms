68320faf8d03d5dcb0504bcc2df4d023ed51b787
Юрий Соломон <tsar.povelevaet@gmail.com>
2022-08-13 13:35:04 +0300

    Добавьте файлы проекта.

---
+++ TwoForms/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TwoForms
+{    
+    public partial class Form1 : Form
+    {
+        private Form2 F2 = null;
+        public Form1()
+        {
+            InitializeComponent(); 
+            F2 = new Form2();
+            F2.Show();
+        }              
+
+        public string Info
+        {
+            get { return textBox1.Text; }
+            set { textBox1.Text = value; }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+           F2.Info = textBox1.Text;
+        }
+    }
+}
---
+++ TwoForms/Form2.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TwoForms
+{
+    public partial class Form2 : Form
+    {
+        private Form1 F1 = null;
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        public string Info
+        {
+            get { return textBox1.Text; }
+            set { textBox1.Text = value; }
+        }
+
+        public Form1 Fa1
+        {
+            get { return F1; }
+            set { F1 = value; }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            F1.Info = textBox1.Text;
+            
+        }
+            
+    }
+}
