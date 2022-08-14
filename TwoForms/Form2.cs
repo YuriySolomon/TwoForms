6627e3497471950f5fa639987e5820318d883bb0
Юрий Соломон <tsar.povelevaet@gmail.com>
2022-08-14 10:47:03 +0300

    Update

--- TwoForms/Form2.cs
+++ TwoForms/Form2.cs
@@ -25,12 +25,6 @@ namespace TwoForms
             set { textBox1.Text = value; }
         }
 
-        //public Form1 Fa1
-        //{
-        //    get { return F1; }
-        //    set { F1 = value; }
-        //}
-
         private void textBox1_TextChanged(object sender, EventArgs e)
         {
             F1.Info = textBox1.Text;
